8c742d017bb0e1edcd67c3ce3df0f35ab9e46740
ProyectoAdrian <82125399+ProyectoAdrian@users.noreply.github.com>
2021-04-07 17:59:27 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyDCU-cE-YfSIUV_QNuDsGM32ZlKyX0PV_s",
+    authDomain: "autentifica43raai-16ad3.firebaseapp.com",
+    projectId: "autentifica43raai-16ad3",
+    storageBucket: "autentifica43raai-16ad3.appspot.com",
+    messagingSenderId: "2690088240",
+    appId: "1:2690088240:web:129af36e9442e1cdc7dcf5",
+    measurementId: "G-ZR80NT40HT"
+    });
