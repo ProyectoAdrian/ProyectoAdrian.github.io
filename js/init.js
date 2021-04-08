04366432a52931bb6ea2f4f0e9942ec17a01df3e
ProyectoAdrian <82125399+ProyectoAdrian@users.noreply.github.com>
2021-04-07 18:09:00 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-   apiKey: "AIzaSyDCU-cE-YfSIUV_QNuDsGM32ZlKyX0PV_s",
-    authDomain: "autentifica43raai-16ad3.firebaseapp.com",
-    projectId: "autentifica43raai-16ad3",
-    storageBucket: "autentifica43raai-16ad3.appspot.com",
-    messagingSenderId: "2690088240",
-    appId: "1:2690088240:web:129af36e9442e1cdc7dcf5",
-    measurementId: "G-ZR80NT40HT"
+   apiKey: "AIzaSyBhYPuzgilL-LcOws0fgCTCJk3Y-mYT5Rs",
+    authDomain: "proyectofinal-3856c.firebaseapp.com",
+    projectId: "proyectofinal-3856c",
+    storageBucket: "proyectofinal-3856c.appspot.com",
+    messagingSenderId: "976943187829",
+    appId: "1:976943187829:web:227c3009e862c1cd0b753c",
+    measurementId: "G-NEG2GXXVXT"
     });
